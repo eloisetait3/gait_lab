ffb6e655dcc7f7d26fe6efe0808a78ecf4771f16
Megan Thresh <meganthresh96@gmail.com>
2020-08-05 14:23:04 +0100

    styling changes to landing page

---
+++ app/javascript/custom/navbar.js
@@ -0,0 +1,4 @@
+function responsiveNavbar() {
+  const nav = document.getElementById('navbar');
+  const title = document.getElementById('nav-title');
+}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// Custom code 
+require("custom/navbar")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -28,12 +30,10 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-import { scrollFunction } from 'packs/navbar_scroll';
  
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  window.onscroll = scrollFunction();
 });
 
 
