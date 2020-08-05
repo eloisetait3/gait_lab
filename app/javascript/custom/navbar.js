8afe7d4c760e8214867a7e4c4b8c1c37a99bd96f
Megan Thresh <meganthresh96@gmail.com>
2020-08-05 16:34:05 +0100

    stylistic changes

--- app/javascript/custom/navbar.js
+++
@@ -1,4 +0,0 @@
-function responsiveNavbar() {
-  const nav = document.getElementById('navbar');
-  const title = document.getElementById('nav-title');
-}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,6 +34,8 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+
+
 });
 
 
