4a0828bd184116f13c2f14696ba68e40a945c536
Megan Thresh <meganthresh96@gmail.com>
2020-08-05 18:23:30 +0100

    Added some javascript, but need to fix features

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,6 +30,7 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
+
  
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
