179fece3598a3dd4594ad7a0ad92da38d4dbb9dd
Megan Thresh <meganthresh96@gmail.com>
2020-08-05 12:49:17 +0100

    Added javascript files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,7 +28,13 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
+import { scrollFunction } from 'packs/navbar_scroll';
+ 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  window.onscroll = scrollFunction();
 });
+
+
+
---
+++ app/javascript/packs/navbar_scroll.js
@@ -0,0 +1,12 @@
+export function scrollFunction(){
+  const prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    const currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
+}
